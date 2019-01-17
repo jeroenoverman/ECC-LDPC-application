f4681ba3443b9234593c4c59649b01f96006120c
Vito Kortbeek <vito.kortbeek@gmail.com>
2019-01-17 15:10:39 +0100

    Clean up AWGN

--- cpp/src/awgn.cpp
+++ cpp/src/awgn.cpp
@@ -11,28 +11,11 @@ void AWGN::apply(double *data, int size, double Eb_N0_dB) {
     /* Setup RNG */
     auto dist = std::bind(std::uniform_real_distribution<double>{0.0,1.0},
                           std::mt19937(std::random_device{}()));
-
-    /* I don't think the complex result is needed */
-#ifdef COMPLEX_COMP
-    std::complex<double> *awgn_signal = new std::complex<double>[size];
-
-    /* Add white gaussian noise, 0dB variance */
-    for (int i=0; i<size; i++) {
-        double t = 1/sqrt(2);
-        std::complex<double> n(t*dist(), t*dist());
-        awgn_signal[i] = data[i] + pow(10, (-1.0 * Eb_N0_dB / 20)) * n;
-
-        /* Extract the real part */
-        data[i] = awgn_signal[i].real();
-    }
-    delete awgn_signal;
-#else
     /* Add white gaussian noise, 0dB variance */
     for (int i=0; i<size; i++) {
         double n = 1/sqrt(2)*dist();
         data[i] = data[i] + pow(10, (-1.0 * Eb_N0_dB / 20)) * n;
     }
-#endif
 }
 
 double AWGN::comp_var(double *data, int size) {
